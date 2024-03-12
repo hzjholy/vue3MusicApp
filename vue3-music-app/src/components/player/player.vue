2b54a9c89347db0f1bf19a992a2425b21b49128b
hzjholy <41406542+hzjholy@users.noreply.github.com>
2024-03-13 05:44:03 +0800

    5.8播放器 cd唱片旋转相关逻辑开发

--- vue3-music-app/src/components/player/player.vue
+++ vue3-music-app/src/components/player/player.vue
@@ -12,6 +12,45 @@
         <h1 class="title">{{ currentSong.name }}</h1>
         <h2 class="subtitle">{{ currentSong.singer }}</h2>
       </div>
+      <div
+        class="middle"
+        @touchstart.prevent="onMiddleTouchStart"
+        @touchmove.prevent="onMiddleTouchMove"
+        @touchend.prevent="onMiddleTouchEnd"
+      >
+        <div class="middle-l" :style="middleLStyle">
+          <div ref="cdWrapperRef" class="cd-wrapper">
+            <div ref="cdRef" class="cd">
+              <img
+                ref="cdImageRef"
+                class="image"
+                :class="cdCls"
+                :src="currentSong.pic"
+              />
+            </div>
+          </div>
+          <div class="playing-lyric-wrapper">
+            <div class="playing-lyric">{{ playingLyric }}</div>
+          </div>
+        </div>
+        <scroll class="middle-r" ref="lyricScrollRef" :style="middleRStyle">
+          <div class="lyric-wrapper">
+            <div v-if="currentLyric" ref="lyricListRef">
+              <p
+                class="text"
+                :class="{ current: currentLineNum === index }"
+                v-for="(line, index) in currentLyric.lines"
+                :key="line.num"
+              >
+                {{ line.txt }}
+              </p>
+            </div>
+            <div class="pure-music" v-show="pureMusicLyric">
+              <p>{{ pureMusicLyric }}</p>
+            </div>
+          </div>
+        </scroll>
+      </div>
       <div class="bottom">
         <div class="dot-wrapper">
           <span class="dot" :class="{ active: currentShow === 'cd' }"></span>
@@ -70,6 +109,7 @@ import { computed, watch, ref } from "vue";
 
 import useMode from "./use-mode";
 import useFavorite from "./use-favorite";
+import useCd from "./use-cd";
 
 import ProgressBar from "./progress-bar.vue";
 import { formatTime } from "@/assets/js/util";
@@ -97,6 +137,7 @@ export default {
     // hooks
     const { modeIcon, changeMode } = useMode();
     const { getFavoriteIcon, toggleFavorite } = useFavorite();
+    const { cdCls, cdRef, cdImageRef } = useCd();
     // computed
     const playlist = computed(() => store.state.playlist);
     const playIcon = computed(() => {
@@ -251,6 +292,10 @@ export default {
       formatTime,
       onProgressChanging,
       onProgressChanged,
+      // cd
+      cdCls,
+      cdRef,
+      cdImageRef
     };
   },
 };
